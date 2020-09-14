e8c32a3d1419703c1425d9a316fd4cf2e9db9adf
majutre <majutrevisan@gmail.com>
2020-09-14 12:11:22 -0300

    Implement chess layer and board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,14 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Board b = new Board(8, 8);
+		ChessMatch match = new ChessMatch();
+		UI.printBoard(match.getPieces());
 		
-	
 	}
 
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -28,5 +28,12 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position p) {
+		return pieces[p.getRow()][p.getColumn()];
+	}
 	
 }
